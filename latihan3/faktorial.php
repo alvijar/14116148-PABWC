e51ee5d55471f4894ea7375b7f2c822a8038bd22
Alvijar <alvijarp11@gmail.com>
2019-10-22 17:12:52 +0700

    latihan3

---
+++ latihan3/faktorial.php
@@ -0,0 +1,7 @@
+<html>
+  <form action="fungsi.php" method="POST">
+    Masukkan bilangan bulat positif :
+    <input name="bilangan" type="text" size="3"><br>
+    <input name="faktorial" type="submit" value="Hitung">
+  </form>
+</html>
