c8b7a963d9ac93830c9613451dc09794ac283ac2
Alvijar <alvijarp11@gmail.com>
2019-10-23 16:02:59 +0700

    Update latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -1,26 +1,27 @@
 <?php
 
-$nama = "Alvijar Akbar Pahlevi";
-$Panjang_nama = strlen($nama);
+function bet($color="red", $nama='Alvijar Akbar Pahlevi'){
 
-if ($Panjang_nama==1 && $Panjang_nama<=10) {
-    $harga_tot = $Panjang_nama*300;
-    echo '<font color=yellow>Nama   : '.$nama.'</font><br>';
-    echo '<font color=yellow>Harga total : Rp.'.$harga_tot.'</font><br>';
-}elseif ($Panjang_nama>10 && $Panjang_nama<=20) {
-    $harga_tot = $Panjang_nama*500;
-    echo '<font color=blue>Nama   : '.$nama.'</font><br>';
-    echo '<font color=blue>Harga total : Rp.'.$harga_tot.'</font><br>';
-}elseif ($Panjang_nama>20) {
-    $harga_tot = $Panjang_nama*700;
-    echo '<font color=purple>Nama   : '.$nama.'</font><br>';
-    echo '<font color=purple>Harga total : Rp.'.$harga_tot.'</font><br>';
-}else {
-    function bet($color="red"){
-        echo '<font color="'.$color.'">Nama : '.$nama.'</font><br>';
-        echo '<font color="'.$color.'">Harga total : Rp.'.$harga_tot.'</font><br>';
+    $Panjang_nama = strlen($nama);
+
+    if ($Panjang_nama==1 && $Panjang_nama<=10) {
+        $harga_tot = $Panjang_nama*300;
+        echo 'Nama   : <font color="'.$color.'">'.$nama.'</font><br>';
+        echo 'Panjang Nama : <font color="'.$color.'">'.$Panjang_nama.'</font><br>';
+        echo 'Harga total : Rp.<font color="'.$color.'">'.$harga_tot.'</font><br>';
+    }elseif ($Panjang_nama>10 && $Panjang_nama<=20) {
+        $harga_tot = $Panjang_nama*500;
+        echo 'Nama   : <font color="'.$color.'">'.$nama.'</font><br>';
+        echo 'Panjang Nama : <font color="'.$color.'">'.$Panjang_nama.'</font><br>';
+        echo 'Harga total : Rp.<font color="'.$color.'">'.$harga_tot.'</font><br>';
+    }elseif ($Panjang_nama>20) {
+        $harga_tot = $Panjang_nama*700;
+        echo 'Nama   : <font color="'.$color.'">'.$nama.'</font><br>';
+        echo 'Panjang Nama : <font color="'.$color.'">'.$Panjang_nama.'</font><br>';
+        echo 'Harga total : Rp.<font color="'.$color.'">'.$harga_tot.'</font><br>';
     }
-    bet();
-}
 
+    
+}
+bet();
 ?>
